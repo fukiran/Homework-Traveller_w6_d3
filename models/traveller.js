cc37b391081a443d9991c2f166fbce527e976f04
fukiran <macdorphoto@yahoo.com>
2021-01-07 18:11:34 +0000

    calculate total distance travelled tested

--- models/traveller.js
+++ models/traveller.js
@@ -29,7 +29,9 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
